928a12230da46bf5c182c9272322359eef354e40
Raymon Drost <ray@raydrost.com>
2024-06-09 08:43:46 -0400

    Removed threading

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pystray
 from pystray import MenuItem as item
 from PIL import Image, ImageDraw
-from threading import Thread
+# from threading import Thread
 from platform import system
 
 from holo import holo_touch
@@ -10,8 +10,9 @@ from any import any_touch
 # Function to create an icon
 def load_image():
     # Generate an image for the icon
-    if system() == "Darwin": return Image.open("logo-black.png")
-    else: return Image.open("logo.png") 
+    # if system() == "Darwin": return Image.open("logo-black.png")
+    # else: return Image.open("logo.png") 
+    return Image.open("logo.png")
 
 # Function to handle the tray menu actions
 def on_clicked(icon, item):
@@ -20,13 +21,14 @@ def on_clicked(icon, item):
             f.write("stop")
     elif item == "Option 2":
         open("stopHolo.txt", "w").close()
-        t1 = Thread(target=holo_touch)
-        t1.start()
-        # holo_touch()
+        # t1 = Thread(target=holo_touch)
+        # t1.start()
+        holo_touch()
     elif item == "Option 3":
-        t2 = Thread(target=any_touch)
-        t2.start()
-        # any_touch()
+        open("stopHolo.txt", "w").close()
+        # t2 = Thread(target=any_touch)
+        # t2.start()
+        any_touch()
 
 # Create the tray icon
 icon = pystray.Icon("test_icon")
